22660ed085270a4651acb4d7394eddc0dc36a0b4
Pockiez23 <ninerocker_2904@hotmail.com>
2026-04-27 13:48:26 +0700

    commet DailyCron

--- internal/config/job.go
+++ internal/config/job.go
@@ -3,5 +3,5 @@ package config
 type Job struct {
 	MasterDataCron string `env:"JOB_MASTER_DATA_CRON,required" envDefault:"*/15 * * * *"`
 	HolidayCron    string `env:"JOB_HOLIDAY_CRON,required" envDefault:"15 3 * * *"`
-	DailyCron      string `env:"JOB_DAILY_CRON" envDefault:"0 0 * * *"`
+	DailyCron      string `env:"JOB_DAILY_CRON" envDefault:"0 0 * * *"` // this is test cron for production 1 minute interval `*/1 * * * *`
 }
